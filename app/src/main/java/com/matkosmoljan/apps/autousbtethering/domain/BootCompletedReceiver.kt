0c53b3d776532ee7af6cdfdc4dafff4d285f5d18
msmoljan <matko.smoljan2@gmail.com>
2019-05-31 23:04:15 +0200

    Fix intent action check in broadcast receiver

--- app/src/main/java/com/matkosmoljan/apps/autousbtethering/domain/BootCompletedReceiver.kt
+++ app/src/main/java/com/matkosmoljan/apps/autousbtethering/domain/BootCompletedReceiver.kt
@@ -3,12 +3,14 @@ package com.matkosmoljan.apps.autousbtethering.domain
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.ACTION_BOOT_COMPLETED
+import android.content.Intent.ACTION_LOCKED_BOOT_COMPLETED
 import com.matkosmoljan.apps.autousbtethering.MainActivity
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
+        if (intent.action == ACTION_LOCKED_BOOT_COMPLETED || intent.action == ACTION_BOOT_COMPLETED) {
             MainActivity
                 .createIntent(context)
                 .let(context::startActivity)
